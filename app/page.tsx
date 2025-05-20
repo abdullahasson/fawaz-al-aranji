fb1456217dbb1faa2ed8c41360162fcaa5581179
abdullahasson <bh532997@gmail.com>
2025-05-20 09:19:52 +0300

    add exercies section

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,72 @@ import Image from "next/image";
 import Footer from "@/components/Footer";
 // Images 
 import Image1 from "@/public/images/fawas-2.jpg";
-import Image2 from "@/public/images/1EwZOnjVpOD7ih.gif";
+import Image2 from "@/public/images/n3FxoNjdlwSzOm.gif";
+import Image3 from "@/public/images/tTaJdXZuxjN3fl.gif";
+import Image4 from "@/public/images/IWxPySm8vh8xwI.gif"
 
 export default function Home() {
+
+
+  const clearExercises = [
+    {
+      "bodyPart": "waist",
+      "equipment": "وزن الجسم",
+      "gifUrl": Image2,
+      "id": "0001",
+      "name": "تمارين الجلوس الثلاثية الربع",
+      "target": "abs",
+      "secondaryMuscles": [
+        "عضلات الفخذين القابضة",
+        "الظهر السفلي"
+      ],
+      "instructions": [
+        "استلقِ على ظهرك مع ثني الركبتين ووضع القدمين على الأرض",
+        "ضع يديك خلف رأسك مع توجيه المرفقين للخارج",
+        "باستخدام عضلات بطنك، ارفع الجزء العلوي من جسمك ببطء عن الأرض مع التكور للأمام حتى يصدر جذعك بزاوية 45 درجة",
+        "اثبت للحظة عند القمة، ثم اخفض الجزء العلوي من جسمك ببطء إلى وضع البداية",
+        "كرر التمرين للعدد المطلوب من المرات"
+      ]
+    },
+    {
+      "bodyPart": "chest",
+      "equipment": "آلة النفوذ",
+      "gifUrl": Image3,
+      "id": "0009",
+      "name": "غطسة الصدر بمساعدة (الركوع)",
+      "target": "pectorals",
+      "secondaryMuscles": [
+        "العضلة ثلاثية الرؤوس",
+        "الأكتاف"
+      ],
+      "instructions": [
+        "اضبط الآلة على الارتفاع المطلوب وثبّت ركبتيك على الوسادة",
+        "أمسك المقابض مع توجيه راحتي اليدين للأسف وذراعيك ممدودتين بالكامل",
+        "اخفض جسمك عن طريق ثني المرفقين حتى تصبح العضد العلوية موازية للأرض",
+        "اثبت للحظة، ثم ادفع نفسك للأعلى إلى وضع البداية",
+        "كرر التمرين للعدد المطلوب من المرات"
+      ]
+    },
+    {
+      "bodyPart": "shoulders",
+      "equipment": "قضيب حديد",
+      "gifUrl": Image4,
+      "id": "0041",
+      "name": "الرفعة الأمامية بقضيب الحديد",
+      "target": "delts",
+      "secondaryMuscles": [
+        "biceps",
+        "triceps"
+      ],
+      "instructions": [
+        "قف مع مباعدة القدمين بعرض الكتفين وأمسك القضيب أمام فخذيك بقبضة علوية.",
+        "ارفعه أماميًا حتى مستوى الكتفين مع إبقاء الذراعين مستقيمتين.",
+        "توقف للحظة في الأعلى، ثم أنزله ببطء.",
+        "كرر للعدد المطلوب."
+      ]
+    },
+  ]
+
   return (
     <div>
       <div>
@@ -92,29 +155,46 @@ export default function Home() {
           <h2 className="title">رحلتك تبدأ من هنا</h2>
         </section>
         <section className="schedules">
-          <h2 className="title">جداول تمارين</h2>
-          <div className="schedules-content">
-            <div className="schedules-card">
-              <div>
-                <Image src={Image2} alt="schedules-image" />
-              </div>
-              <h3>دوران</h3>
-            </div>
-            <div className="schedules-card">
-              <div>
-                <Image src={Image2} alt="schedules-image" />
-              </div>
-              <h3>دوران</h3>
-            </div>
-            <div className="schedules-card">
-              <Image src={Image2} alt="schedules-image" />
-            </div>
-            <div className="schedules-card">
-              <Image src={Image2} alt="schedules-image" />
-            </div>
+          <h2 className="title">تمارين الكترونية واضحة</h2>
+          <div className="flex flex-wrap gap-2">
+            {
+              clearExercises.map((item) => (
+                <div className="exercise-card" key={item.id}>
+                  <div className="card-header">
+                    <h1 className="exercise-title">{item.name}</h1>
+                    <div className="exercise-info">
+                      <span>المستوى: {item.target}</span>
+                      <span>ID: {item.id}</span>
+                    </div>
+                  </div>
+                  <div className="card-body">
+                    <div className="gif-container">
+                      <Image src={item.gifUrl} alt="gif" className="gif-iframe" />
+                    </div>
+                    <div className="muscles-section">
+                      <h3 className="section-title">العضلات المستخدمة</h3>
+                      <ul className="muscles-list">
+                        {item.secondaryMuscles.map((muscle) => (
+                          <li key={muscle}>{muscle}</li>
+                        ))}
+                      </ul>
+                    </div>
+                    <div className="instructions-section">
+                      <h3 className="section-title">الخطوات</h3>
+                      <ol className="instructions-list">
+                        {item.instructions.map((instruction) => (
+                          <li key={instruction}>{instruction}</li>
+                        ))}
+                      </ol>
+                    </div>
+                    <div className="equipment-chip">{item.equipment}</div>
+                  </div>
+                </div>
+
+              ))
+            }
           </div>
         </section>
-
 
         <Footer />
       </div>
